366809e01bbe062a345e1e5710f1938e3a4aab6f
christhomas1190 <111439861+christhomas1190@users.noreply.github.com>
2025-03-05 11:19:06 -0500

    suffix complete

--- src/main/java/com/zipcodewilmington/danny_do_better_exercises/StringUtilities.java
+++ src/main/java/com/zipcodewilmington/danny_do_better_exercises/StringUtilities.java
@@ -46,7 +46,8 @@ public class StringUtilities {
      * @return the last 3 characters of `input`
      */
     public static String getSuffix(String input){
-        return null;
+
+        return input.substring(input.length()-3, input.length());
     }
 
     /**
