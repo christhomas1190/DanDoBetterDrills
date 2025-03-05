e76790651dc2354fc2e8c7b0603aefbfda1003d5
christhomas1190 <111439861+christhomas1190@users.noreply.github.com>
2025-03-05 08:36:14 -0500

    finished lessthan or equal too

--- src/main/java/com/zipcodewilmington/danny_do_better_exercises/PredicateUtilities.java
+++ src/main/java/com/zipcodewilmington/danny_do_better_exercises/PredicateUtilities.java
@@ -30,7 +30,8 @@ public class PredicateUtilities {
      * @return true if `x` is greater than or equal to `y`
      */
     public Boolean isGreaterThanOrEqualTo(int x, int y) {
-        return null;
+
+        return x>=y;
     }
 
     /**
